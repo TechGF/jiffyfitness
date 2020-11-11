54592a3f9074816bd84bb0ab84db2225aa6c50f4
jonathan leibovici <jonathanleibovici@gmail.com>
2020-11-10 21:46:28 -0500

    initial commit

---
+++ JIFFY FITNESS/JIFFY FITNESS/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  JIFFY FITNESS
+//
+//  Created by Jonathan Leibovici on 11/10/20.
+//  Copyright © 2020 JIFFY FITNESS. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JIFFY FITNESS/JIFFY FITNESS/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  JIFFY FITNESS
+//
+//  Created by Jonathan Leibovici on 11/10/20.
+//  Copyright © 2020 JIFFY FITNESS. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var userNameField: UITextField!
+    
+    
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func OnSignUp(_ sender: Any) {
+        
+    }
+    
+    
+    
+    @IBAction func OnLogin(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
