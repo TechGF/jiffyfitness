9ebe7cb4b87ca2cb98cb9c364da06d5e71b700a3
Gary Feng <garyfeng961@gmail.com>
2020-11-19 16:05:51 -0500

    add buttons

--- JIFFY FITNESS/JIFFY FITNESS/FeedViewController.swift
+++ JIFFY FITNESS/JIFFY FITNESS/FeedViewController.swift
@@ -19,31 +19,16 @@ class FeedViewController: UIViewController {
     @IBOutlet weak var lowerImage: UIImageView!
     
     
-    /*
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        upperImage.isUserInteractionEnabled = true
-        midImage.isUserInteractionEnabled = true
-        lowerImage.isUserInteractionEnabled = true
-        
-        upperImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imageTap)))
 
-        lowerImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imageTap)))
 
         
 
         // Do any additional setup after loading the view.
     }
-    @objc func imageTap() {
-        // present modally
-        self.present(UpperBodyViewController(), animated: true)
-        // or push to the navigation stack
-        //self.navigationController?.push(UpperBodyViewController())
-        // or perform segue if you use storyboards
-        self.performSegue(withIdentifier: "UpperBodyViewController", sender: "")
-    }
- */
+
+ 
     /*
     // MARK: - Navigation
 
